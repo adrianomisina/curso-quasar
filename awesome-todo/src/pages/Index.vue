46cfeddc4fa99c876886660b27fc46c8a49499c5
Adriano Misina <adriano.misina@gmail.com>
2021-09-18 10:22:19 -0300

    new commit

---
+++ awesome-todo/src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page padding>
+    <input type="text" 
+    v-model="message" 
+    @keyup.esc ="clearMessage"
+    @keyup.enter = "alertMessage">
+    <button @click="clearMessage">Clear</button>
+    <h5>{{message}}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message:'I love Vue.JS so hard!'
+    }
+  },
+
+  methods: {
+    clearMessage() {
+      this.message = '';
+    },
+
+    alertMessage() {
+      alert(this.message)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
